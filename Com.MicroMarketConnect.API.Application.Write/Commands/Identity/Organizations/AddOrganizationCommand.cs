ca95353aec6771955ac9ae2a8e0a6626113e4899
Alexandre CANOEN <alexandre@acanoen.fr>
2025-12-07 12:30:08 +0100

    feat: Adding restriction on creation

--- Com.MicroMarketConnect.API.Application.Write/Commands/Identity/Organizations/AddOrganizationCommand.cs
+++ Com.MicroMarketConnect.API.Application.Write/Commands/Identity/Organizations/AddOrganizationCommand.cs
@@ -1,6 +1,8 @@
-﻿using Com.MicroMarketConnect.API.Core;
+﻿using Com.MicroMarketConnect.API.Application.Write.Ports;
+using Com.MicroMarketConnect.API.Core;
 using Com.MicroMarketConnect.API.Core.Orchestration;
 using Com.MicroMarketConnect.API.Core.Ports;
+using Com.MicroMarketConnect.API.Core.Validation;
 using Com.MicroMarketConnect.API.Domain.IdentityModule.Aggregates;
 using Com.MicroMarketConnect.API.Domain.IdentityModule.Aggregates.Enums;
 using Com.MicroMarketConnect.API.Domain.IdentityModule.Organization;
@@ -15,11 +17,18 @@ public record AddOrganizationCommand(
     string? Description) : IEventDrivenCommand;
 
 public class AddOrganizationCommandHandler(
+    IOrganizationCommandRepository repository,
     IUserProvider userProvider,
     IGuidProvider guidProvider) : CommandHandler<AddOrganizationCommand>
 {
     protected override async Task<Result<IReadOnlyCollection<IDomainEvent>>> Handle(AddOrganizationCommand command)
     {
+        var organizationExists = await repository.Exists(command.Name);
+        if (organizationExists is { IsFailed: true, Errors: var errors })
+            return Result.Fail(errors);
+        if (organizationExists.Value)
+            return Result.Fail(Errors.Conflict);
+
         var id = RowId.Hydrate(guidProvider.NewGuid());
         var userId = RowId.Hydrate(userProvider.GetId());
 
